a069c10178e18d1bf60283d750bdb2d52b0d48dc
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-08-25 12:13:14 +1000

    06: очистка public + смена на HashRouter + build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import BlogDetails from './BlogDetails';
 import NotFound from './NotFound';
 
